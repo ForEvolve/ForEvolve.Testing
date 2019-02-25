1ed8f0c92501dc6e777faaebd6e7b84df1f49724
Carl-Hugo Marcotte <carlhugo@gmail.com>
2019-02-25 00:13:53 -0500

    Add more extensions

---
+++ src/ForEvolve.Testing.AspNetCore/Identity/ServiceCollectionAuthorizationExtensions.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using System.Collections.Generic;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    public static class ServiceCollectionAuthorizationExtensions
+    { 
+        public static IServiceCollection ByPassPolicies(this IServiceCollection services, IEnumerable<string> policiesName)
+        {
+            var authorizedPolicy = CreateAuthorizedPolicy();
+            return services.PostConfigure<AuthorizationOptions>(options =>
+            {
+                foreach (var policyName in policiesName)
+                {
+                    options.AddPolicy(policyName, authorizedPolicy);
+                }
+                options.DefaultPolicy = authorizedPolicy;
+            });
+        }
+
+        public static IServiceCollection ByPassDefaultPolicy(this IServiceCollection services)
+        {
+            var authorizedPolicy = CreateAuthorizedPolicy();
+            return services.PostConfigure<AuthorizationOptions>(options =>
+            {
+                options.DefaultPolicy = authorizedPolicy;
+            });
+        }
+
+        public static IServiceCollection ReplacePolicy(this IServiceCollection services, string policyName, AuthorizationPolicy policy)
+        {
+            return services.PostConfigure<AuthorizationOptions>(options =>
+            {
+                options.AddPolicy(policyName, policy);
+            });
+        }
+
+        private static AuthorizationPolicy CreateAuthorizedPolicy()
+        {
+            return new AuthorizationPolicyBuilder()
+                .RequireAssertion(x => true)
+                .Build();
+        }
+    }
+
+}
