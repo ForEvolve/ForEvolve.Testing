aaa5bb74570056b76e76201563af22070cb35c5a
Carl-Hugo Marcotte <carlhugo@gmail.com>
2019-02-25 00:18:21 -0500

    Add HTTP helpers

---
+++ src/ForEvolve.Testing.AspNetCore/Http/HttpContextHelperOptions.cs
@@ -0,0 +1,15 @@
+﻿namespace ForEvolve.Testing.AspNetCore.Http
+{
+    public class HttpContextHelperOptions
+    {
+
+        //public string AuthorizationBearerToken { get; set; }
+        //public string ExpectedAuthorizationHeader
+        //{
+        //    get
+        //    {
+        //        return $"Bearer {AuthorizationBearerToken}";
+        //    }
+        //}
+    }
+}
